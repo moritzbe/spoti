514cb4615d6950113a3f0aec606afb3f61a3d7dd
moritzbe <moritzberthold@hotmail.com>
2015-02-13 10:58:01 +0100

    application.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,35 @@
 'use strict';
 
 module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            // 2. Configuration for concatinating files goes here.
+            application: {
+              src: ['src/javascript/*.js'],
+              dest: 'dist/application.js',
+            },
+
+            styles: {
+              src: ['src/styles/*.css'],
+              dest: 'dist/styles.css',
+            }
+            // extras: {
+            //   src: ['src/main.js', 'src/extras.js'],
+            //   dest: 'dist/with_extras.js',
+            // },
+        }
+
+    });
+
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.   
+  grunt.registerTask('default', ['concat']);
   // Project configuration.
 
   // Load the plugin tasks
