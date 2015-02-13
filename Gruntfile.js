2d857fda0b6fe4c7b0aa0ea3cb72148d2eb42d8d
moritzbe <moritzberthold@hotmail.com>
2015-02-13 11:28:12 +0100

    grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,28 +10,58 @@ module.exports = function(grunt) {
             // 2. Configuration for concatinating files goes here.
             application: {
               src: ['src/javascript/*.js'],
-              dest: 'dist/application.js',
+              dest: 'build/application.js',
             },
 
             styles: {
               src: ['src/styles/*.css'],
-              dest: 'dist/styles.css',
+              dest: 'build/styles.css',
             }
             // extras: {
             //   src: ['src/main.js', 'src/extras.js'],
             //   dest: 'dist/with_extras.js',
             // },
+        },
+        uglify: {
+          build: {
+              src: 'build/application.js',
+              dest: 'build/application.min.js'
+          }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['src/images/*.{png,jpg,gif}'],
+                    dest: 'build/images'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/javascript/*.js', 'src/styles/*.css', 'src/images/*.{png,jpg,gif}', 'src/index.html'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            } 
         }
+
 
     });
 
   // 3. Where we tell Grunt we plan to use this plug-in.
   grunt.loadNpmTasks('grunt-contrib-concat');
-
-  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.   
-  grunt.registerTask('default', ['concat']);
-  // Project configuration.
-
+  // Uglify
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   // Load the plugin tasks
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.   
+  grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
 };
